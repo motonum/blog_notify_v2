3416142d0da11390bb2e0e6bb43cfc837f99c790
motonum <73626946+motonum@users.noreply.github.com>
2025-08-29 15:51:06 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+const ATOM_URL = "";
+const SITE_URL = "";
+
+const DEBUG_MODE = true;
+
+const TEST_WEBHOOK_URL = "";
+
+const MAIN_WEBHOOK_URL = "";
+
+const WEBHOOK_URL = DEBUG_MODE ? TEST_WEBHOOK_URL : MAIN_WEBHOOK_URL;
+
+// アクセス先の情報
+const USER_ID = "";
+const PASSWORD = "";
+
+// GETメソッドのオプション
+const options = {
+  method: "GET",
+  headers: {
+    Authorization: " Basic " + Utilities.base64Encode(USER_ID + ":" + PASSWORD),
+  },
+  muteHttpExceptions: true,
+};
+
+class ExDate extends Date {
+  toFormattedString() {
+    return this.toLocaleString("ja-JP", {
+      month: "short",
+      day: "numeric",
+      weekday: "short",
+    });
+  }
+  omitTime() {
+    this.setHours(0);
+    this.setMinutes(0);
+    this.setSeconds(0);
+    this.setMilliseconds(0);
+    return this;
+  }
+  shiftDate(days = 1) {
+    this.setDate(this.getDate() + days);
+    return this;
+  }
+}
+
+function refreshTrigger() {
+  const allTriggers = ScriptApp.getProjectTriggers();
+  allTriggers
+    .filter((trigger) => trigger.getHandlerFunction() === "main")
+    .forEach((trigger) => {
+      ScriptApp.deleteTrigger(trigger);
+    });
+  const now = new Date();
+  const next = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0);
+  ScriptApp.newTrigger("main").timeBased().at(next).create();
+}
+
+function postDiscord(message) {
+  UrlFetchApp.fetch(WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    payload: JSON.stringify({ content: `${message}` }),
+  });
+}
+
+function main() {
+  const atomXml = UrlFetchApp.fetch(ATOM_URL, options).getContentText();
+  const document = XmlService.parse(atomXml);
+  const root = document.getRootElement();
+  const ns = XmlService.getNamespace("http://purl.org/atom/ns#");
+  const entries = root.getChildren("entry", ns);
+  const latest = entries
+    .map((entry) => {
+      return {
+        issued: new Date(entry.getChild("issued", ns).getText()),
+        title: entry.getChild("title", ns).getText(),
+        summary: entry
+          .getChild("summary", ns)
+          .getText()
+          .replace(/\&.+\;/g, ""),
+        url: entry.getChild("id", ns).getText(),
+      };
+    })
+    .reduce((prev, curr) => (prev.issued > curr.issued ? prev : curr));
+
+  const summary = latest.summary.replace(/\s+/g, " ");
+
+  postDiscord(`### ${latest.title}\n${summary}\n${latest.url}`);
+}
