cbc0bfe92d9764443a11f7c5497765f96164e0f9
motonum <73626946+motonum@users.noreply.github.com>
2025-08-31 14:00:09 +0900

    improve: bin/push.shですべての変更がコミットされていたら自動でbuildとgithubおよびgasへのpushを行うように変更

--- bin/push.sh
+++ bin/push.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-bin/build.sh
-bin/deploy.sh
+if [[ -z `git status -s` ]]; then
+  echo "exec 'git push'"
+  git push
+  echo "exec 'bin/build.sh'"
+  bin/build.sh
+  echo "exec 'bin/deploy.sh'"
+  bin/deploy.sh
+else
+  echo "Error: Please commit all changes."
+fi
