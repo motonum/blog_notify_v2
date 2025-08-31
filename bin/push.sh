aac80d7f9076dcbbb3167240e7705a25d73eb28e
motonum <73626946+motonum@users.noreply.github.com>
2025-08-31 15:19:16 +0900

    fix: エラー時に終了するように変更

--- bin/push.sh
+++ bin/push.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname $0)/.."
 
 if [[ -z `git status -s` ]]; then
+  echo "exec 'bin/build.sh' $1"
+  bin/build.sh $1
   echo "exec 'git push'"
   git push
-  echo "exec 'bin/build.sh' $1"
-  bin/build.sh
   echo "exec 'bin/deploy.sh'"
   bin/deploy.sh
 else
